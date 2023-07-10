f044d20511da3cf2b072cb8fa76b35309d65a9a2
aadhivenkat3 <sharmavenkat1@gmail.com>
2023-07-10 22:29:12 +0530

    add

---
+++ main.js
@@ -0,0 +1,54 @@
+//string start//
+
+let name="venkatesh";
+let profession='Developer';
+let native=`PCM`;
+
+console.log(typeof native)
+console.log(name,profession,native);
+
+//string end//
+
+
+//Number start//
+
+let num1=20;
+let num2=2.5;
+
+console.log(num1,num2);
+
+//Number end//
+
+
+//Bigint start//
+
+
+let lng1=123456789n
+let lng2=1234556789n
+
+let result=lng1+lng2
+console.log(result);
+
+
+//Bigint end//
+
+//Boolean start//
+
+confirm();
+
+//Boolean end//
+
+//undefined start//
+
+let bharath;
+console.log(bharath);
+
+
+//undefined end//
+
+
+//Null start//
+const number=null;
+
+
+//Null End//
